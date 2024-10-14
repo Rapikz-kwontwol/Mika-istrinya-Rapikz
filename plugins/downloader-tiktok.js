a9cece8c744cc376dc8f5a0bc387be405fdf827c
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-10-14 19:22:59 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 if (!text) throw `Linknya Mana?`
 m.reply(wait)
 try {
-let anu = await fetch(`https://widipe.com/download/ttdl?url=${text}`)
+let anu = await fetch(`https://widipe.com/download/v2/ttdl?url=${text}`)
 let result = await anu.json()
 await conn.sendFile(m.chat, result.result.video, 'anu.mp4', `*Description:* ${result.result.title}`, m)
 await conn.sendMessage(m.chat, {
