b3c4ca584b59f73975559185166b5c02a090e454
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-10-14 14:34:32 +0700

    Add files via upload

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,16 +1,82 @@
 import fetch from 'node-fetch'
-
- let handler = async (m, { conn, text, usedPrefix, command }) => {
-   if (!text) throw `*Example:* ${usedPrefix}${command} <link yt>`;
-conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
- let ouh = await fetch(`https://api.ssateam.my.id/api/aio?query=${text}`)
-  let gyh = await ouh.json() 
-	let url = `${gyh.data.response.url}`
-	conn.sendFile(m.chat, url, null, `NIH KAK VIDEONYA`, m)
+const {
+    proto,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia
+  } = (await import('@adiwajshing/baileys')).default
+import { googleImage } from '@bochilteam/scraper'
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Use example ${usedPrefix}${command} https://youtube.com/xxxxx`
+    try {
+const data = {
+    title: "KLIK TOMBOL INI",
+    sections: [{
+            title: `DOWNLOADER YOUTUBE VIDEO`,
+            rows: [{
+                    title: "240p",
+                    description: "BURIK NJIR😹",
+                    id: `.ytv240p ${text}`
+                },
+                {
+                    title: "360p",
+                    description: "BURIK JUGA CUK😹",
+                    id: `.ytv360p ${text}`
+                },
+                            {
+                    title: "480p",
+                    description: "HD KAGA BURIK JUGA KAGA",
+                    id: `.ytv480p ${text}`
+                },
+                {
+                    title: "720p",
+                    description: "HD BJIR😹",
+                    id: `.ytv720p ${text}`
+                },
+            ]
+        }
+    ]
 }
-handler.help = handler.command = ['ytshorts', 'shorts', 'short', 'ytmp4', 'ytv'];
-handler.tags = ['downloader'];
-handler.limit = true;
-handler.premium = false;
+let msgs = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: "Klik Tombol dibawah ini untuk memilih kualitas Video"
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "DOWNLOADER YOUTUBE VIDEO"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: `Link ${text}`,
+            subtitle: "DOWNLOADER",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [{
+                "name": "single_select",
+                "buttonParamsJson": JSON.stringify(data)
+              }],
+          })
+       })
+    }
+  }
+}, { quoted: m })
 
-export default handler;
+return await conn.relayMessage(m.chat, msgs.message, {})
+} catch (e) {
+conn.sendFile(m.chat, eror, "anu.mp3", null, m, true, {
+		type: "audioMessage",
+		ptt: true,
+	})
+}}
+handler.help = ['ytv']
+handler.tags = ['downloader']
+handler.command = /^(ytv)$/i
+handler.limit = false
+handler.register = true
+
+export default handler
